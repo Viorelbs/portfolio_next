c2bba4b19420acacf6698c7b9d8dafd0b1c2f67f
ViorelMG5 <viorel@mg5.ro>
2023-05-20 11:20:37 +0300

    removed bugged framer animation

--- components/Layout/HeroContent.tsx
+++ components/Layout/HeroContent.tsx
@@ -9,28 +9,12 @@ export default function HeroContent() {
   return (
     <div className="relative min-h-[90vh] grid place-content-center hero mx-4">
       <div className="bg-secondary p-6 rounded-xl">
-        <motion.div
-          variants={{
-            initial: {
-              y: -200,
-            },
-            normal: {
-              y: 0,
-            },
-          }}
-          initial="initial"
-          animate="normal"
-          transition={{
-            duration: 0.5,
-          }}
-        >
-          <h1 className="text-3xl font-medium">
-            Hi there! <br />
-            My name is <span className="text-primary">Viorel</span>, and I'm{" "}
-            <br />A <span className="text-primary">web developer</span> and{" "}
-            <span className="text-primary">UI/UX designer</span>{" "}
-          </h1>
-        </motion.div>
+        <h1 className="text-3xl font-medium">
+          Hi there! <br />
+          My name is <span className="text-primary">Viorel</span>, and I'm{" "}
+          <br />A <span className="text-primary">web developer</span> and{" "}
+          <span className="text-primary">UI/UX designer</span>{" "}
+        </h1>
       </div>
 
       <ContactDrawer buttonType="primary" />
